7db071ed2b25068b1ef0804a0f367dd3afb8305b
TheLongFranz <65250674+TheLongFranz@users.noreply.github.com>
2026-07-27 21:20:00 +0100

    UPDATE: Test

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -41,7 +41,7 @@ TEST_CASE("open(): fails with non-standard baud rates", "[serial]") {
   auto port{waysurs::serial_port()};
 
   const auto baud_rates =
-      GENERATE(as<std::uint32_t>{}, 0, 42, 42, 451, 123'456'789);
+      GENERATE(as<std::uint32_t>{}, 0, 42, 451, 123'456'789);
   REQUIRE(!(port.open({.port_name = port_tx_name, .baud_rate = baud_rates})));
 }
 
