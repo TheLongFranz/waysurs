98708475c3cb34375f67982d2ffbaf8794b125cf
TheLongFranz <65250674+TheLongFranz@users.noreply.github.com>
2026-08-06 22:05:39 +0100

    docs(is_open)

--- include/waysurs/waysurs.hpp
+++ include/waysurs/waysurs.hpp
@@ -60,7 +60,8 @@ public:
     /// complete I'll replace this with delete("reason")
     serial_port& operator=(const serial_port& other) = delete;
 
-    [[nodiscard]] auto is_open() -> bool;
+    [[nodiscard]] auto is_open() -> bool; ///< returns true if the port has been opened and there is
+                                          ///< a valid config, false otherwise
 
     /// @param config - config struct defining port configuration
     /// @note returns a no-op success if the config hasn't changed, void on success, error on
